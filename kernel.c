82c3b5b3de7c99ab1444244a5834875267a842b5
tomlay0520 <122665664+tomlay0520@users.noreply.github.com>
2025-09-18 19:39:28 +0800

    tutorial1

--- kernel.c
+++ kernel.c
@@ -1,8 +1,10 @@
 extern void uart0_init(void);
 extern void uart0_put_string(char *s);
 
+
 void start_kernel(void)
 {
+    // kernel的中文是：内核
     uart0_init();
     uart0_put_string("Hello, SUEP!\n");
     uart0_put_string("suep@risc-v$ ");
