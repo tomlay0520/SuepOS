00cf98921df8332d3d4d567f0664e46aac2564d7
tomlay0520 <122665664+tomlay0520@users.noreply.github.com>
2025-09-19 12:56:56 +0800

    WRONG_MESSAGE: virtio0内存映射的地址写错了。

--- hardware_conf.h
+++ hardware_conf.h
@@ -4,6 +4,27 @@
 
 #define MAX_CPU 8
 
+
+
+// memory layout:
+//     0x00000000 --> +-----------+
+//                    |   BIOS    |
+//     0x00001000 --> +-----------+
+//                    | boot code |
+//     0x02000000 --> +-----------+
+//                    |   CLINT   |
+//     0x0C000000 --> +-----------+
+//                    |   PLIC    |
+//     0x10000000 --> +-----------+
+//                    |   UART0   |
+//     0x10001000 --> +-----------+
+//                    |  VIRTIO0  |
+//     0x80000000 --> +-----------+
+//                    |           |
+//                    |  KERNEL   |
+//                    |           |
+//                    +-----------+
+
 // Memory-mapped I/O addresses for the UART:
 #define UART0 0x10000000L
 // Main memory size is as follows:
