093b8003b676a95a36f935fa87cfc3652ae821e1
tomlay0520 <122665664+tomlay0520@users.noreply.github.com>
2025-09-23 17:12:11 +0800

    opt01感觉头很痛

--- usr_mode.c
+++ usr_mode.c
@@ -2,26 +2,28 @@
 extern void process_exit(void);
 
 void test_task01(void){
-    while(1) {
+    // while(1) {
         mini_printf("test01\n");
         delay(5000);
         process_give_up(); 
-    }
+        process_exit();
+    // }
 }
 
 void test_task02(void){
-    while(1) {
+    // while(1) {
         mini_printf("test02\n");
         delay(5000);
         process_give_up();
-    }
+        process_exit();
+    // }
 }
 
 void test_task03(void){
-    while(1) {
+    // while(1) {
         mini_printf("hello world!\n");
         delay(5000);
         process_give_up();
-        // process_exit();
-    }
+        process_exit();
+    // }
 }
